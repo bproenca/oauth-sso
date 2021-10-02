010fd4244d6ebb638532d37991aca120133a033c
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-10-02 17:45:12 -0300

    first commit

---
+++ resource-server/src/main/java/br/com/bcp/Controller.java
@@ -0,0 +1,40 @@
+package br.com.bcp;
+
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Controller.class);
+
+    @GetMapping("/api/private")
+    public Map<String, Object> privateResource(@AuthenticationPrincipal Jwt jwt) {
+        LOG.info("\n***** JWT Headers: {}\n", jwt.getHeaders());
+        LOG.info("\n***** JWT Claims: {}\n", jwt.getClaims().toString());
+        LOG.info("\n***** JWT Token: {}\n", jwt.getTokenValue());
+        LOG.info("\n***** JWT Audience: {}\n", jwt.getAudience());
+        LOG.info("\n***** JWT Expires at: {}\n", jwt.getExpiresAt());
+        LOG.info("\n***** JWT Issuer: {}\n", jwt.getIssuer());
+
+        return jwt.getClaims();
+    }
+
+    @GetMapping("/user/info")
+    public Map<String, Object> getUserInfo(@AuthenticationPrincipal Jwt principal) {
+        return Collections.singletonMap("user_name", principal.getClaimAsString("preferred_username"));
+    }
+
+    @GetMapping("/api/public")
+    public String publicResource() {
+        return "Public resource " + LocalTime.now();
+    }
+}
---
+++ resource-server/src/main/java/br/com/bcp/SecurityConfig.java
@@ -0,0 +1,32 @@
+package br.com.bcp;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+    http
+      .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/actuator/**")
+            .hasRole("actuadoradmin")
+        .antMatchers(HttpMethod.GET, "/api/public/**")
+            .permitAll()
+        .anyRequest()
+            .authenticated()
+        .and()
+            .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+  }
+  
+}
