189f604a295296fa214aed73d1c4f47505d86ac0
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-10-04 10:03:43 -0300

    app1

---
+++ app1-client/src/test/java/br/com/bcp/app1client/App1ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bcp.app1client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App1ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
